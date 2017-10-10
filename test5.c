d6aff0be52839d16a7423d44fbfa081b324c5436
SanketKhobragade <cs15btech11020@iith.ac.in>
2017-10-10 16:56:48 +0530

    Add files via upload

---
+++ test5.c
@@ -0,0 +1,26 @@
+int main(){
+	int a=3,b=4,c=5,d=6,e,f,g;
+	if(a==3){
+		if(b==4){
+			e=0;
+		}
+		else{
+			e=1;
+		}
+		f=15;
+	}
+	else{
+		if(c==5){
+			e=2;
+			if(d==5){
+				e=10;
+			}
+			else{
+				f=9;
+			}
+		}
+		else{
+			e=20;
+		}
+	}
+}
